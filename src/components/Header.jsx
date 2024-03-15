888d8f003ee7f448371910f80ea6b974ae93053e
JangHwanPark <wkd5878@gmail.com>
2024-03-15 22:19:36 +0900

    [MarkUp] Navbar MarkUp

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <img src="/img/logo_lexus@2x.png" alt="Lexus Korea Logo"/>
+            </div>
+            <nav>
+                <ul>
+                    <li>MODEL</li>
+                    <li>BUY</li>
+                    <li>SERVICE</li>
+                    <li>BRAND</li>
+                </ul>
+                <ul>
+                    <li>시승신청</li>
+                    <li>LEXUSLOUNGE</li>
+                    <li>LOGIN</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
