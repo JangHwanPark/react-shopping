3c7fdda2927775758d755971d0c4bf0ca0d4505a
JangHwanPark <wkd5878@gmail.com>
2024-03-18 22:48:01 +0900

    [Feat] Carousel Slider 뷰포트 설정 (요소 3개 개당 100vw)

--- src/components/CarouselSlide.jsx
+++ src/components/CarouselSlide.jsx
@@ -3,8 +3,8 @@ import styles from './CarouselSlide.module.css';
 
 export default function CarouselSlide(props) {
     return (
-        <div className={styles.carousel_container}>
-            <div className={styles.carousel_wrapper}>
+        <div className={styles.carousel_viewport}>
+            <div className={styles.carousel_container}>
                 <div className={styles.carousel_item}>CarouselSlide1</div>
                 <div className={styles.carousel_item}>CarouselSlide2</div>
                 <div className={styles.carousel_item}>CarouselSlide3</div>
