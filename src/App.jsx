9f6f686c29d23aed4659bc598cf3d864d3d1983f
JangHwanPark <wkd5878@gmail.com>
2024-03-20 22:29:02 +0900

    [Feat] Create Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header";
 import {Outlet} from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <div className="container">
             <Header/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -5,6 +5,11 @@ export default function Home(props) {
     return (
         <div>
             <CarouselSlide/>
+            <div>
+                <div>img1</div>
+                <div>img2</div>
+                <div>img3</div>
+            </div>
         </div>
     );
 }
