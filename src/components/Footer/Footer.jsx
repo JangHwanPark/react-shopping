f04334d5d025addc56c9092ebbbd77f33ccc5386
JangHwanPark <wkd5878@gmail.com>
2024-04-18 18:25:08 +0900

    todo : Footer before 추가 (구분선)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,6 +9,7 @@ export default function Footer() {
                     <div>렉서스 서비스 예약 080-4500-4500 (24시간 연중무휴)</div>
                     <div>고객 지원실 080-4300-4300 (평일 09:00-18:00)</div>
                 </section>
+                {/* Todo: before 추가 (구분선) */}
                 <section className={'section_relation'}>
                     <span>개인정보 처리방침</span>
                     <span>법적고지</span>
