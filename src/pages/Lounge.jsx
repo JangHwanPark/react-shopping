d71547951b0fac79231906fc2c1067d5e28dde7c
JangHwanPark <wkd5878@gmail.com>
2024-04-18 09:13:57 +0900

    feat: Lounge 스켈레톤 작업

--- src/pages/Lounge.jsx
+++ src/pages/Lounge.jsx
@@ -3,19 +3,24 @@ import Title from "../components/Title/Title";
 
 export default function Lounge() {
     return (
-        <main>
+        <main className={'lounge_container'}>
             <Title titleText={'LEXUS LOUNGE'}/>
-            <p>
-                LEXUS LOUNGE는 렉서스 고객만을 위한
-                프리미엄 멤버십 서비스입니다.
-                애플리케이션과 웹사이트를 통해 렉서스 고객만을 위한
-                차량 유지와 관리에 필요한 정보를 확인하고
-                몇 번의 터치만으로 내게 필요한 서비스를 경험해 보세요.
-
-                라이프스타일을 중요시하는 당신에게 어울리는 프리미엄 혜택 또한
-                LEXUS LOUNGE에 준비되어 있습니다.
-            </p>
-            <div>
+            <div className={'lounge_content'}>
+                <p>
+                    LEXUS LOUNGE는 렉서스 고객만을 위한
+                    프리미엄 멤버십 서비스입니다.
+                </p>
+                <p>
+                    애플리케이션과 웹사이트를 통해 렉서스 고객만을 위한
+                    차량 유지와 관리에 필요한 정보를 확인하고
+                    몇 번의 터치만으로 내게 필요한 서비스를 경험해 보세요.
+                </p>
+                <p>
+                    라이프스타일을 중요시하는 당신에게 어울리는 프리미엄 혜택 또한
+                    LEXUS LOUNGE에 준비되어 있습니다.
+                </p>
+            </div>
+            <div className={'notice-wrap'}>
                 <h2>유의사항</h2>
                 <p>렉서스 라운지의 이미지는 고객의 이해를 돕기 위해 연출된 것으로 실제와 차이가 있을 수 있습니다.</p>
                 <p>렉서스 라운지 서비스 관련 자세한 사항은 렉서스 라운지 고객 지원실 통해 문의 바랍니다.</p>
