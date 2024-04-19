c764823d5ee28c6e006f8c9fd3ee1d559e92caf4
JangHwanPark <wkd5878@gmail.com>
2024-04-19 10:24:02 +0900

    feat: NotFound Design & MarkUp

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
-export default function NotFound(props) {
+export default function NotFound() {
     return (
-        <div>NotFound</div>
+        <div className="error-page">
+            <div className="error-content">
+                <h1 className="error-code">404</h1>
+                <p className="error-message">Error: 요청하신 페이지를 찾을 수 없습니다.</p>
+                <Link to="/" className="home-link">Return to Home</Link>
+            </div>
+        </div>
     );
 }
