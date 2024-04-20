a3678a44e4d94b078dca4acd7cf62ba0471ccc5c
JangHwanPark <wkd5878@gmail.com>
2024-04-20 19:45:36 +0900

    refactoring : user 데이터중 필요한 데이터만 추출 및 출력

--- src/pages/MyAccount/MyAccount.jsx
+++ src/pages/MyAccount/MyAccount.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useAuth} from "../../context/AuthContext";
 import {useNavigate} from "react-router-dom";
 import SectionLayout from "../../components/SectionLayout/SectionLayout";
 import UnorderedList from "../../components/UnorderedList/UnorderedList";
 import AccountLayout from "../../layout/AccountLayout";
-import {userInfo, vehicleDetails} from "../../data/my_account.js";
 
 export default function MyAccount() {
     const {user} = useAuth();
     const navigate = useNavigate();
-    if (!user) navigate("/login");
+
+    // 로그인이 되어 있지 않다면 로그인 페이지로 이동
+    useEffect(() => {
+        if (!user) navigate("/login");
+    }, [user, navigate]);
+
+    // 필요한 사용자 정보만 추출
+    const userDetails = user ? {
+        '이름': user.displayName,
+        '이메일': user.email,
+        '휴대폰 번호': user.phoneNumber || '전화번호 없음' // 전화번호가 없는 경우 대체 텍스트
+    } : {};
+
+    // userDetails를 배열로 변환 (UnorderedList 컴포넌트가 배열만 받음)
+    const userDetailsArray = Object.entries(userDetails).map(([key, value]) => ({ key, value }));
 
     return (
         <AccountLayout user={user}>
             <SectionLayout title={'사용자 정보'} className={'section_account'}>
-                <UnorderedList className={'account-details'} listItems={userInfo}/>
+                <UnorderedList
+                    className={'account-details'}
+                    listItems={userDetailsArray}
+                />
             </SectionLayout>
         </AccountLayout>
     );
--- src/pages/MyAccount/MyCar.jsx
+++ src/pages/MyAccount/MyCar.jsx
@@ -1,28 +1,65 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useAuth} from "../../context/AuthContext";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import AccountLayout from "../../layout/AccountLayout";
 import SectionLayout from "../../components/SectionLayout/SectionLayout";
 import UnorderedList from "../../components/UnorderedList/UnorderedList";
-import {vehicleDetails} from "../../data/my_account.js";
 
 export default function MyCar() {
     const {user} = useAuth();
     const navigate = useNavigate();
-    if (!user) navigate("/login");
+
+    // 로그인이 되어 있지 않다면 로그인 페이지로 이동
+    useEffect(() => {
+        if (!user) navigate("/login");
+    }, [user, navigate]);
+
+    // 차량 정보가 있는지 확인하는 flag
+    const hasVehicleDetails = user && user.vehicleDetails;
+    
+    // 최근 정비 내역이 있는지 확인하는 flag
+    const hasMaintenanceHistory = user && user.maintenanceHistory;
+    
+    // 필요한 사용자 정보만 추출
+    const userDetails = user ? {
+        '대표 차량': user.phoneNumber,
+        '차량 별명': user.phoneNumber,
+        '차량 번호': user.phoneNumber,
+        '모델명': user.phoneNumber,
+        '차대 번호': user.phoneNumber,
+        '출고 일자': user.phoneNumber,
+    } : {};
+
+    // userDetails를 배열로 변환 (UnorderedList 컴포넌트가 배열만 받음)
+    const userDetailsArray = Object.entries(userDetails).map(([key, value]) => ({ key, value }));
 
     return (
         <AccountLayout user={user}>
             <SectionLayout title={'사용자 보유 차량'} className={'section_account'}>
-                <UnorderedList className={'account-details'} listItems={vehicleDetails}/>
+                {hasVehicleDetails ? (
+                    <UnorderedList
+                        className={'account-details'}
+                        listItems={userDetailsArray}
+                    />
+                ) : (
+                    <div>
+                        <p>차량이 등록되어 있지 않습니다.</p>
+                        {/*<Link to="/vehicle-register" className="button">차량 등록하기</Link>*/}
+                        <p onClick={() => alert('준비중 입니다.')}>차량 등록하기</p>
+                    </div>
+                )}
             </SectionLayout>
             <SectionLayout title={'최근 정비 내역'} className={'section_account'}>
-                <address>
-                    서비스 센터명: [센터명]<br/>
-                    담당 SA: [SA명]<br/>
-                    전화: [전화번호]<br/>
-                    위치: [위치]
-                </address>
+                {hasMaintenanceHistory ? (
+                    <address>
+                        서비스 센터명: [센터명]<br/>
+                        담당 SA: [SA명]<br/>
+                        전화: [전화번호]<br/>
+                        위치: [위치]
+                    </address>
+                ) : (
+                    <p>최근 정비내역이 존재하지 않습니다.</p>
+                )}
             </SectionLayout>
         </AccountLayout>
     );
