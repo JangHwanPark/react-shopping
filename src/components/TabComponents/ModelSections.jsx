bed05315ae34c86b24106b0a9ea0ed3d031d5f76
JangHwanPark <wkd5878@gmail.com>
2024-04-21 19:10:59 +0900

    feat: 사용자가 선택한 옵션, 선택완료 탭에 출력 구현

--- src/components/TabComponents/ModelSections.jsx
+++ src/components/TabComponents/ModelSections.jsx
@@ -108,12 +108,12 @@ export function InteriorSection({ onInteriorChange }) {
 // 선택완료 섹션 컴포넌트
 export function FinalizeSection({ modelId, selectedExterior, selectedInterior, modelGrade, estimatedPrice }) {
     return (
-        <div className={'model_finalize'}>
+        <div className={'model_finalize flex-column'}>
             <h2>선택완료</h2>
             <p>모델 이름: {modelId}</p>
             <p>모델 등급: {modelGrade}</p>
-            <p>익스테리어 색상: {selectedExterior}</p>
-            <p>인테리어 색상: {selectedInterior}</p>
+            <p>익스테리어: {selectedExterior}</p>
+            <p>인테리어: {selectedInterior}</p>
             <p>예상 금액: {estimatedPrice}</p>
         </div>
     );
