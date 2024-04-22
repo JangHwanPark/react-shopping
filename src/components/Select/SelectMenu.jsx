eed31621f722835fee6d884b5671697133c25d74
JangHwanPark <wkd5878@gmail.com>
2024-04-22 09:47:30 +0900

    feat: 데이터 베이스 연결 및 시승 데이터 저장 테스트

--- src/components/Select/SelectMenu.jsx
+++ src/components/Select/SelectMenu.jsx
@@ -22,14 +22,13 @@ import React from 'react';
  *     addDaySuffix={false}
  * />
  */
-export default function SelectMenu({ className, value, onChange, options, defaultOption, ...rest }) {
-    const { addDaySuffix} = rest;
-    console.log('SelectMenu: ', options)
+export default function SelectMenu({className, options, name, value, onChange, defaultOption, addDaySuffix}) {
+    console.log('SelectMenu name: ', name)
     return (
-        <select className={className} value={value} onChange={onChange}>
+        <select className={className} value={value} name={name} onChange={onChange}>
             {/* defaultOption 이 제공되면, 사용자가 다른 옵션을 선택하기 전에 표시할 기본 옵션을 추가 */}
             {defaultOption && (
-                <option value="">{defaultOption}</option>
+                <option value="" >{defaultOption}</option>
             )}
 
             {/* options 배열을 순회하여, 각 옵션을 <option> 태그로 변환 */}
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,22 +1,14 @@
 import React, {useState} from 'react';
-import ColorList from "../components/ColorList/ColorList";
-import models from "../data/models.json";
-import SelectMenu from "../components/Select/SelectMenu";
+import useAddData from "../hooks/useAddData";
+import Modal from "../components/ModalComponents/Modal";
 
 export default function TestPage() {
-    const modelId = 'ES 300h'
-    // 모든 모델을 옵션으로 변환하고, 특정 모델 ID를 첫 번째 옵션으로 설정
-    const options = [
-        { value: modelId, label: modelId },
-        ...models.map(({ name }) => ({ value: name, label: name }))
-    ];
+
+
+
     return (
         <div>
-            <SelectMenu
-                id="test_drive_show_rooms1"
-                className="select_show_rooms"
-                options={options}
-            />
+
         </div>
     );
 }
